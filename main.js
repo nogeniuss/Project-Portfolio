f7f467455f5d2d87a5c65510177acb356242ccff
nogeniuss <nogeniuss@gmail.com>
2024-11-18 21:30:08 -0300

    comit 2

---
+++ main.js
@@ -0,0 +1,152 @@
+//css
+function updateProgressBarCss(percentage) {
+    const progressBar = document.querySelector('.progress-css');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-css');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarCss(85);
+
+  //js
+function updateProgressBarJS(percentage) {
+    const progressBar = document.querySelector('.progress-js');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-js');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarJs(64);
+
+  //html
+function updateProgressBarHtml(percentage) {
+    const progressBar = document.querySelector('.progress-html');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-html');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarHtml(95);
+
+
+
+  //---
+
+  //node
+function updateProgressBarCss(percentage) {
+    const progressBar = document.querySelector('.progress-css');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-css');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarCss(37);
+
+  //docker
+function updateProgressBarJS(percentage) {
+    const progressBar = document.querySelector('.progress-js');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-js');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarJs(74);
+
+  //git
+function updateProgressBarGit(percentage) {
+    const progressBar = document.querySelector('.progress-git');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-git');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarGit(95);
+
+  //--
+
+  //github
+function updateProgressBarGith(percentage) {
+    const progressBar = document.querySelector('.progress-github');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-github');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarGith(98);
+
+  //python
+function updateProgressBarPy(percentage) {
+    const progressBar = document.querySelector('.progress-py');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-py');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarPy(79);
+
+  //linux
+function updateProgressBarLinux(percentage) {
+    const progressBar = document.querySelector('.progress-linux');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-linux');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarLinux(50);
+
+
+  //--
+
+  //sql
+function updateProgressBarSql(percentage) {
+    const progressBar = document.querySelector('.progress-sql');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-sql');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarSql(97);
+
+  //supabase
+function updateProgressBarSupa(percentage) {
+    const progressBar = document.querySelector('.progress-supa');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-supa');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarSupa(73);
+
+  //bubble
+function updateProgressBarBubble(percentage) {
+    const progressBar = document.querySelector('.progress-bubble');
+    progressBar.style.width = percentage + '%';
+  
+    const percentageText = document.querySelector('.percentage-bubble');
+    percentageText.textContent = percentage + '%';
+  }
+  
+  // Chamando a função com o valor desejado (exemplo: 70%)
+  updateProgressBarBubble(99);
